bdaa34d1fcff1c499a05a94a2c5f5a83ff4ddfda
AdamMohsen4 <adam.amohsen4@gmail.com>
2025-03-27 12:04:17 +0000

    Deploying to gh-pages from @ AdamMohsen4/e-parcel@374dd5a05b332aa910b05451e2ee52949e6d68cc 🚀

--- assets/separator-DUgnfTJS.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as r,b9 as o}from"./vendor-react-c2Vhr46D.js";import{c as e}from"./index-C-QwnRat.js";const s=a.forwardRef((({className:a,orientation:s="horizontal",decorative:i=!0,...t},l)=>r.jsx(o,{ref:l,decorative:i,orientation:s,className:e("shrink-0 bg-border","horizontal"===s?"h-[1px] w-full":"h-full w-[1px]",a),...t})));s.displayName=o.displayName;export{s as S};
